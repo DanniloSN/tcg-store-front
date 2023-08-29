eaa7d4de1a8e0d3ede146c036c949d4045cb8aca
Danilo de Souza Nunes <dannilosn@hotmai.com>
2023-08-29 17:11:47 -0300

    Interface renamed

--- src/components/molecules/delimiter/index.tsx
+++ src/components/molecules/delimiter/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-interface PropsDelimiter {
+interface DelimiterProps {
   children: React.ReactNode
   padding?: boolean
 }
 
-export default function Delimiter(props: PropsDelimiter) {
+export default function Delimiter(props: DelimiterProps) {
   return (
     <div className={`delimiter ${props.padding && 'p-2'}`}>
       {props.children}
